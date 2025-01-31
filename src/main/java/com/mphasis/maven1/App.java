646c8bf2775888be7b5cd3f3598523534ee5085e
Thappeta Reddy <thappeta.reddy@mphasis.com>
2025-01-31 12:11:54 +0530

    Hi I am Neeraj and this is my first code

---
+++ src/main/java/com/mphasis/maven1/App.java
@@ -0,0 +1,21 @@
+package com.mphasis.maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data)
+	{
+		System.out.print("Data"+ data);
+		if(data.equals("Neeraj"))
+		{
+			System.out.println("Welcome");
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
